589129384fb7a0f4705c69a06ad494610bb45d10
Bharat <18uec169@lnmiit.ac.in>
2020-07-08 17:22:41 +0530

    assignment-1

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 
 app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId',dishRouter);
+
+app.use('/promotions',promoRouter);
+app.use('/promotions/:promoId',promoRouter);
+
+app.use('/leaders',leaderRouter);
+app.use('/leaders/:leaderId',leaderRouter);
+
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
